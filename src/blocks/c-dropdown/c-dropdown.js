a128b0ef7a55d95478167ebea375303557d901e4
Elmira Ilyasova <capricorn_26@mail.ru>
2022-03-20 10:45:12 +0700

    Add date dropdown components

--- src/blocks/c-dropdown/c-dropdown.js
+++ src/blocks/c-dropdown/c-dropdown.js
@@ -57,14 +57,6 @@ const getFacilitiesTitle = (data) => {
 const dropdowns = document.querySelectorAll('.c-dropdown');
 
 dropdowns.forEach((dropdown)=> {
-  const title = dropdown.querySelector('.c-dropdown__title');
-  const list = dropdown.querySelectorAll('.c-dropdown__item');
-  const header = dropdown.querySelector('.c-dropdown__header');
-  const main = dropdown.querySelector('.c-dropdown__main');
-  const cleanButton = dropdown.querySelector('.c-dropdown__clean');
-  const confirmButton = dropdown.querySelector('.c-dropdown__confirm');
-  
-  const data = {};
   let type = null;
   if (dropdown.classList.contains('c-dropdown--facilities')) {
     type = 'facilities';
@@ -73,85 +65,96 @@ dropdowns.forEach((dropdown)=> {
     type = 'guests';
   }
 
-  header.onclick = (() => {
-    const isOpen = dropdown.classList.contains('open') ? true : false;
-    if (isOpen) {
-      dropdown.classList.remove('open');
-      main.style.display = 'none';
-    } 
-    else {
-      dropdown.classList.add('open');
-      main.style.display = 'block';
-    }
-  });
+  if (type == 'facilities' || type == 'guests') {
+    const title = dropdown.querySelector('.c-dropdown__title');
+    const list = dropdown.querySelectorAll('.c-dropdown__item');
+    const header = dropdown.querySelector('.c-dropdown__header');
+    const main = dropdown.querySelector('.c-dropdown__main');
+    const cleanButton = dropdown.querySelector('.c-dropdown__clean');
+    const confirmButton = dropdown.querySelector('.c-dropdown__confirm');
+    
+    const data = {};
+    
 
-  list.forEach((item) => {
-    const name = item.querySelector('.c-dropdown__item-title');
-    const decrementButton = item.querySelector('.c-dropdown__decrement');
-    const incrementButton = item.querySelector('.c-dropdown__increment');
-    const count = item.querySelector('.c-dropdown__count');
-
-    data[name.textContent] = count.textContent;
-
-    const changeTitle = () => {
-      data[name.textContent] = count.textContent;
-      if (type === 'guests') {
-        title.textContent = getGuestsTitle(data);
+    header.onclick = (() => {
+      const isOpen = dropdown.classList.contains('open') ? true : false;
+      if (isOpen) {
+        dropdown.classList.remove('open');
+        main.style.display = 'none';
+      } 
+      else {
+        dropdown.classList.add('open');
+        main.style.display = 'block';
       }
-      if (type === 'facilities') {
-        const facilitiesTitle = getFacilitiesTitle(data)
-        title.textContent = facilitiesTitle.text;
-        if (facilitiesTitle.hint !== null) {
-          title.setAttribute('title', facilitiesTitle.hint);
-        }
-      }
-    }
-
-    decrementButton.onclick = (() => {
-      count.textContent --;
-      data[name.textContent] = count.textContent;
-      if (count.textContent == 0) {
-        decrementButton.disabled = true;
-
-        if (Object.values(data).every(value => value == 0)) {
-          cleanButton.classList.add('hidden');
-        }
-      }
-      changeTitle();
     });
 
-    incrementButton.onclick = (() => {
-      count.textContent ++;
+    list.forEach((item) => {
+      const name = item.querySelector('.c-dropdown__item-title');
+      const decrementButton = item.querySelector('.c-dropdown__decrement');
+      const incrementButton = item.querySelector('.c-dropdown__increment');
+      const count = item.querySelector('.c-dropdown__count');
+
       data[name.textContent] = count.textContent;
-      if (decrementButton.disabled = true) {
-        decrementButton.disabled = false;
+
+      const changeTitle = () => {
+        data[name.textContent] = count.textContent;
+        if (type === 'guests') {
+          title.textContent = getGuestsTitle(data);
+        }
+        if (type === 'facilities') {
+          const facilitiesTitle = getFacilitiesTitle(data)
+          title.textContent = facilitiesTitle.text;
+          if (facilitiesTitle.hint !== null) {
+            title.setAttribute('title', facilitiesTitle.hint);
+          }
+        }
       }
-      
-      cleanButton.classList.contains('hidden') ? cleanButton.classList.remove('hidden') : '';
-      changeTitle();
-    });
-  })
 
-  if (type === 'guests') {
-    console.log('Условие  гости соблюдено');
-    cleanButton.onclick = () => {
-      list.forEach((item) => {
-        const count = item.querySelector('.c-dropdown__count');
-        const decrementButton = item.querySelector('.c-dropdown__decrement');
+      decrementButton.onclick = (() => {
+        count.textContent --;
+        data[name.textContent] = count.textContent;
+        if (count.textContent == 0) {
+          decrementButton.disabled = true;
 
-        count.textContent = 0;    
-        title.textContent = 'Сколько гостей';             
-        decrementButton.disabled = true;
+          if (Object.values(data).every(value => value == 0)) {
+            cleanButton.classList.add('hidden');
+          }
+        }
+        changeTitle();
       });
-      for (value in data) {
-        data[value] = 0;
-      }
-      cleanButton.classList.add('hidden');
-    };
 
-    confirmButton.onclick = () => {
-      dropdown.classList.remove('open');
-      main.style.display = 'none';
-    };
+      incrementButton.onclick = (() => {
+        count.textContent ++;
+        data[name.textContent] = count.textContent;
+        if (decrementButton.disabled = true) {
+          decrementButton.disabled = false;
+        }
+        
+        cleanButton.classList.contains('hidden') ? cleanButton.classList.remove('hidden') : '';
+        changeTitle();
+      });
+    })
+
+    if (type === 'guests') {
+      cleanButton.onclick = () => {
+        list.forEach((item) => {
+          const count = item.querySelector('.c-dropdown__count');
+          const decrementButton = item.querySelector('.c-dropdown__decrement');
+
+          count.textContent = 0;    
+          title.textContent = 'Сколько гостей';             
+          decrementButton.disabled = true;
+        });
+        for (value in data) {
+          data[value] = 0;
+        }
+        cleanButton.classList.add('hidden');
+      };
+
+      confirmButton.onclick = () => {
+        dropdown.classList.remove('open');
+        main.style.display = 'none';
+      };
+    }
   }
 });
